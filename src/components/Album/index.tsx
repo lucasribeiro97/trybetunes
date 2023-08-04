3dae2925e7bdd94e038fa3544a80c5408e98f76a
lucas <lucasriibeiro23@gmail.com>
2023-08-04 13:47:10 -0300

    corrigindo lint

--- src/components/Album/index.tsx
+++ src/components/Album/index.tsx
@@ -4,7 +4,6 @@ import getMusics from '../../services/musicsAPI';
 import Loading from '../Loading';
 import MusicCard from '../MusicCard';
 import { AlbumType, SongType } from '../../types';
-import './album.css';
 
 export default function Album() {
   const [loading, setLoading] = useState(true);
